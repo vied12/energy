2412b0d41d116d084b1fa94aa7321bc6978443ff
Edouard <edou4rd@gmail.com>
2015-02-04 18:11:05 +0100

    directive

--- src/app/main/main.directives.js
+++ src/app/main/main.directives.js
@@ -7,12 +7,11 @@ angular.module('energy')
                 el.css('height', $window.innerHeight - 100 + 'px');
                 scope.$emit('resize');
             }
-            function cleanup() {
-                angular.element($window).off('resize', setHeight);
-            }
             $timeout(setHeight);
             angular.element($window).on('resize', setHeight);
-            el.on('$destroy', cleanup);
+            el.on('$destroy', function cleanup() {
+                angular.element($window).off('resize', setHeight);
+            });
         };
         return {
             restrict: 'A',
