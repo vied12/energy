f424e2a29e83251020e9885cc397f3753bbb3eca
Edouard <edou4rd@gmail.com>
2015-03-14 12:49:17 +0100

    services

--- src/app/main/main.directives.js
+++ src/app/main/main.directives.js
@@ -2,11 +2,11 @@
 
 angular.module('energy')
     .directive('heightLikeWindow', ['$timeout', '$window', '$rootScope', '$state', function ($timeout, $window, $rootScope, $state) {
-        var offset = 0;
-        if ($state.is("editor")) {
-            offset = 200;
-        }
         var link = function (scope, el) {
+            var offset = 0;
+            if ($state.is("editor")) {
+                offset = 200;
+            }
             function setHeight() {
                 el.css('height', $window.innerHeight - offset + 'px');
                 $rootScope.$broadcast('resize');
